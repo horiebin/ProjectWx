3d52eeea3f569ee0bb4206338481f985c0fced67
husonchen <1092441751@qq.com>
2017-04-26 18:20:20 +0800

    用server name区分

--- dao/server_config.py
+++ dao/server_config.py
@@ -1,5 +1,6 @@
 from db_helper import DbHelper
 from util.class_decorator import singleton
+from config import Config
 
 @singleton
 class ServerConfigDao(object):
@@ -7,12 +8,14 @@ class ServerConfigDao(object):
     def __init__(self):
         self.table = 'server_config'
         self.db = DbHelper()
+        self.nameSpace = Config()['server']
         self.config = {}
         self.loadServerConfig()
 
+
     def loadServerConfig(self):
         # load config into memery
-        val = {'name_space':'global'}
+        val = {'name_space':self.nameSpace}
         rows = self.db.select(self.table,where='name_space=$name_space',vars=val)
         for row in rows:
             self.config[row.k] = row.v
@@ -23,7 +26,7 @@ class ServerConfigDao(object):
 
     def set_access_token(self,token):
         self.config['access_token'] = token
-        val = {'name_space': 'global', 'k': 'access_token'}
+        val = {'name_space': self.nameSpace, 'k': 'access_token'}
         self.db.update(self.table,where='name_space=$name_space and k=$k',vars=val,
                        v=token)
 
@@ -32,12 +35,12 @@ class ServerConfigDao(object):
 
     def set_jsapi_ticket(self,jsTicket):
         self.config['jsapi_ticket'] = jsTicket
-        val = {'name_space': 'global', 'k': 'jsapi_ticket'}
+        val = {'name_space': self.nameSpace, 'k': 'jsapi_ticket'}
         self.db.update(self.table,where='name_space=$name_space and k=$k',vars=val,
                        v=jsTicket)
 
     def getGlobalByKey(self,k):
-        val = {'name_space':'global','k':k}
+        val = {'name_space':self.nameSpace,'k':k}
         row = self.db.select(self.table,where='name_space=$name_space and k=$k',vars=val)[0]
         return row.v
 
