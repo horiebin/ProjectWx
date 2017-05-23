ca42eafd4ee0711ad7324f0abd0c83ca147f4566
husonchen <1092441751@qq.com>
2017-05-23 21:22:18 +0800

    增加一个配置

--- config.py
+++ config.py
@@ -16,8 +16,20 @@ class Config(object):
         'appSecret': '',
     }
 
-    config_server = {
+    config_sickworm_server = {
         'server': 'we.xwpay.sickworm.com',
+        'dbn': 'mysql',
+        'username': 'xiaob',
+        'password': 'skdfjkasdf',
+        'dbname': 'xiaob',
+        'host': 'localhost',
+        'port': 3306,
+        'appId': '',
+        'appSecret': '',
+    }
+
+    config_mmd666_server = {
+        'server': 'wx.mmd666.cn',
         'dbn': 'mysql',
         'username': 'xiaob',
         'password': 'skdfjkasdf',
@@ -44,10 +56,13 @@ class Config(object):
         myname = socket.getfqdn(socket.gethostname())
         if myname == 'iZ94mm9cb9hZ':
             print('server is running on %s ... configurating..' % myname)
-            self.config = self.config_server
+            self.config = self.config_sickworm_server
+        elif myname == 'iZwz9gbi9f8xfazznivbhiZ':
+            print('server is running on %s ... configurating..' % myname)
+            self.config = self.config_mmd666_server
         else:
             print('server is running on develop mod...configurating..')
-            self.config = self.config_develop
+            self.config = self.config_mmd666_server
 
         # self.config['appId'] = ServerConfigDao().getGlobalByKey('app_id')
         # self.config['appSecret'] = ServerConfigDao().getGlobalByKey('app_secret')
