271c1622e7673891c7c434fb911d7d3484f0572e
husonchen <1092441751@qq.com>
2017-06-04 21:38:27 +0800

    bug fix

--- cron/flush_autoreply.py
+++ cron/flush_autoreply.py
@@ -1,6 +1,9 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 from dao.auto_reply import AutoReplyDao
-
-
 
 if __name__ == "__main__":
     AutoReplyDao().saveReplysToMc()
